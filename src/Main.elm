a9f8a14ee8a628a8485fc97892ee65c1464e8bd9
Tito Pandu Brahmanto <tito@pandubrahmanto.com>
2017-01-17 17:59:47 +0700

    Reformat elm code

--- src/Main.elm
+++ src/Main.elm
@@ -1,3 +1,5 @@
+module Main exposing (..)
+
 import Html exposing (Html)
 import Html.Attributes exposing (classList)
 import Time exposing (Time, second)
@@ -5,82 +7,107 @@ import Date exposing (Date)
 import Debug exposing (log)
 import PrayTime exposing (..)
 
+
 main =
-  Html.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
-type alias Model = Time
 
-init : (Model, Cmd Msg)
+type alias Model =
+    Time
+
+
+init : ( Model, Cmd Msg )
 init =
-  (0, Cmd.none)
+    ( 0, Cmd.none )
+
 
 type Msg
-  = Tick Time
+    = Tick Time
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Tick newTime ->
-      (newTime, Cmd.none)
+    case msg of
+        Tick newTime ->
+            ( newTime, Cmd.none )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every second Tick
+    Time.every second Tick
+
 
 view : Model -> Html Msg
 view model =
-  let
-      date = Date.fromTime model
-      latitude = -6.1744444
-      longitude = 106.8294444
-      elevation = 0
-      timeZone = toFloat 7
-      htmlTimes =
-        date
-          |> calculatePrayTimes latitude longitude elevation timeZone
-          |> adjustTimes longitude timeZone
-          |> formatTimes
-          |> toTimeList
-          |> List.map htmlTimeStructure
-      htmlClock =
-        date
-          |> formattedDate
-          |> toHtmlClock
-      htmlTimeKeeper =
-        ([ htmlClock ]) ++ (htmlTimes)
+    let
+        date =
+            Date.fromTime model
 
-  in
-     Html.div [ classList [ ("time", True) ] ] htmlTimeKeeper
+        latitude =
+            -6.1744444
+
+        longitude =
+            106.8294444
+
+        elevation =
+            0
+
+        timeZone =
+            toFloat 7
+
+        htmlTimes =
+            date
+                |> calculatePrayTimes latitude longitude elevation timeZone
+                |> adjustTimes longitude timeZone
+                |> formatTimes
+                |> toTimeList
+                |> List.map htmlTimeStructure
+
+        htmlClock =
+            date
+                |> formattedDate
+                |> toHtmlClock
+
+        htmlTimeKeeper =
+            ([ htmlClock ]) ++ (htmlTimes)
+    in
+        Html.div [ classList [ ( "time", True ) ] ] htmlTimeKeeper
 
 
 formattedDate : Date -> String
 formattedDate date =
-  let
-      hour = Date.hour date
-      minute = Date.minute date
-      second = Date.second date
-  in
-     twoDigitsFormat (hour) ++ ":" ++ twoDigitsFormat (minute) ++ ":" ++ twoDigitsFormat (second)
+    let
+        hour =
+            Date.hour date
+
+        minute =
+            Date.minute date
+
+        second =
+            Date.second date
+    in
+        twoDigitsFormat (hour) ++ ":" ++ twoDigitsFormat (minute) ++ ":" ++ twoDigitsFormat (second)
+
 
 toHtmlClock : String -> Html msg
 toHtmlClock clock =
-  Html.div
-  [ classList [ ("time__clock", True) ] ]
-  [ Html.h1 [] [ Html.text (clock) ] ]
+    Html.div
+        [ classList [ ( "time__clock", True ) ] ]
+        [ Html.h1 [] [ Html.text (clock) ] ]
 
-htmlTimeStructure (label, time) =
-  Html.div
-  [ classList [ ("time__shalat", True) ] ]
-  [ Html.div
-    [ classList [ ("time__label", True) ] ]
-    [ Html.text (label) ]
-  , Html.div
-    [ classList [ ("time__shalat-time", True) ] ]
-    [ Html.text (time) ]
-  ]
 
+htmlTimeStructure ( label, time ) =
+    Html.div
+        [ classList [ ( "time__shalat", True ) ] ]
+        [ Html.div
+            [ classList [ ( "time__label", True ) ] ]
+            [ Html.text (label) ]
+        , Html.div
+            [ classList [ ( "time__shalat-time", True ) ] ]
+            [ Html.text (time) ]
+        ]
--- src/PrayTime.elm
+++ src/PrayTime.elm
@@ -3,272 +3,374 @@ module PrayTime exposing (..)
 import Date exposing (Date)
 
 
-toTimeList : FormattedPrayTimes -> List (String, String)
+toTimeList : FormattedPrayTimes -> List ( String, String )
 toTimeList formattedTimes =
-  [ ("Subuh", formattedTimes.fajr)
-  , ("Terbit", formattedTimes.sunRise)
-  , ("Zuhur", formattedTimes.dhuhr)
-  , ("Ashar", formattedTimes.asr)
-  , ("Magrib", formattedTimes.magrib)
-  , ("Isya", formattedTimes.isya)
-  ]
+    [ ( "Subuh", formattedTimes.fajr )
+    , ( "Terbit", formattedTimes.sunRise )
+    , ( "Zuhur", formattedTimes.dhuhr )
+    , ( "Ashar", formattedTimes.asr )
+    , ( "Magrib", formattedTimes.magrib )
+    , ( "Isya", formattedTimes.isya )
+    ]
+
 
 type alias PrayTimes =
-  { fajr: Float
-  , sunRise: Float
-  , dhuhr: Float
-  , asr: Float
-  , magrib: Float
-  , isya: Float
-  }
+    { fajr : Float
+    , sunRise : Float
+    , dhuhr : Float
+    , asr : Float
+    , magrib : Float
+    , isya : Float
+    }
+
 
 calculatePrayTimes : Float -> Float -> Float -> Float -> Date -> PrayTimes
 calculatePrayTimes latitude longitude elevation timeZone date =
-  let
-      julianDate =
-        julianDateByCoordinate longitude (toJulianDate date)
-      fajrTime =
-        sunAngleTime latitude 20 julianDate 0.20833333333333334 CCW
-      sunRiseTime =
-        sunAngleTime latitude (riseSetAngle (elevation)) julianDate 0.25 CCW
-      dhuhrTime =
-        midDayTime julianDate 0.5
-      asrTime =
-        calcAsrTime latitude 1 julianDate 0.5416666666666666
-      magribTime =
-        sunAngleTime latitude 1 julianDate 0.75 None
-      isyaTime =
-        sunAngleTime latitude 18 julianDate 0.75 None
-  in
-     { fajr = fajrTime
-     , sunRise = sunRiseTime
-     , dhuhr = dhuhrTime
-     , asr = asrTime
-     , magrib = magribTime
-     , isya = isyaTime
-     }
+    let
+        julianDate =
+            julianDateByCoordinate longitude (toJulianDate date)
+
+        fajrTime =
+            sunAngleTime latitude 20 julianDate 0.20833333333333334 CCW
+
+        sunRiseTime =
+            sunAngleTime latitude (riseSetAngle (elevation)) julianDate 0.25 CCW
+
+        dhuhrTime =
+            midDayTime julianDate 0.5
+
+        asrTime =
+            calcAsrTime latitude 1 julianDate 0.5416666666666666
+
+        magribTime =
+            sunAngleTime latitude 1 julianDate 0.75 None
+
+        isyaTime =
+            sunAngleTime latitude 18 julianDate 0.75 None
+    in
+        { fajr = fajrTime
+        , sunRise = sunRiseTime
+        , dhuhr = dhuhrTime
+        , asr = asrTime
+        , magrib = magribTime
+        , isya = isyaTime
+        }
 
 
 type alias FormattedPrayTimes =
-  { fajr: String
-  , sunRise: String
-  , dhuhr: String
-  , asr: String
-  , magrib: String
-  , isya: String
-  }
+    { fajr : String
+    , sunRise : String
+    , dhuhr : String
+    , asr : String
+    , magrib : String
+    , isya : String
+    }
+
 
 formatTimes : PrayTimes -> FormattedPrayTimes
 formatTimes prayTimes =
-  { fajr    = formatTime prayTimes.fajr
-  , sunRise = formatTime prayTimes.sunRise
-  , dhuhr   = formatTime prayTimes.dhuhr
-  , asr     = formatTime prayTimes.asr
-  , magrib  = formatTime prayTimes.magrib
-  , isya    = formatTime prayTimes.isya
-  }
+    { fajr = formatTime prayTimes.fajr
+    , sunRise = formatTime prayTimes.sunRise
+    , dhuhr = formatTime prayTimes.dhuhr
+    , asr = formatTime prayTimes.asr
+    , magrib = formatTime prayTimes.magrib
+    , isya = formatTime prayTimes.isya
+    }
+
 
 formatTime time =
-  let
-      roundedTime = fixHour (time + (0.5 / 60))
-      hours = floor (roundedTime)
-      minutes = floor ((roundedTime - toFloat (hours)) * 60)
-  in
-     twoDigitsFormat (hours) ++ ":" ++ twoDigitsFormat (minutes)
+    let
+        roundedTime =
+            fixHour (time + (0.5 / 60))
+
+        hours =
+            floor (roundedTime)
+
+        minutes =
+            floor ((roundedTime - toFloat (hours)) * 60)
+    in
+        twoDigitsFormat (hours) ++ ":" ++ twoDigitsFormat (minutes)
+
 
 twoDigitsFormat : Int -> String
 twoDigitsFormat number =
-  if number < 10 then
-     "0" ++ toString (number)
-  else
-     toString (number)
+    if number < 10 then
+        "0" ++ toString (number)
+    else
+        toString (number)
+
 
 adjustTimes : Float -> Float -> PrayTimes -> PrayTimes
-adjustTimes longitude timeZone {fajr,sunRise,dhuhr,asr,magrib,isya} =
-  let
-      fajrTime =
-        fajr + timeZone - (longitude / 15) + (2 / 60)
-      sunRiseTime =
-        sunRise + timeZone - (longitude / 15) + (-2 / 60)
-      dhuhrTime =
-        (dhuhr + timeZone - (longitude / 15)) + (2 / 60)
-      asrTime =
-        (asr + timeZone - (longitude / 15)) + (2 / 60)
-      magribTime =
-        (magrib + timeZone - (longitude / 15)) + (2 / 60)
-      isyaTime =
-        (isya + timeZone - (longitude / 15)) + (2 / 60)
-  in
-     { fajr = fajrTime
-     , sunRise = sunRiseTime
-     , dhuhr = dhuhrTime
-     , asr = asrTime
-     , magrib = magribTime
-     , isya = isyaTime
-     }
+adjustTimes longitude timeZone { fajr, sunRise, dhuhr, asr, magrib, isya } =
+    let
+        fajrTime =
+            fajr + timeZone - (longitude / 15) + (2 / 60)
+
+        sunRiseTime =
+            sunRise + timeZone - (longitude / 15) + (-2 / 60)
+
+        dhuhrTime =
+            (dhuhr + timeZone - (longitude / 15)) + (2 / 60)
+
+        asrTime =
+            (asr + timeZone - (longitude / 15)) + (2 / 60)
+
+        magribTime =
+            (magrib + timeZone - (longitude / 15)) + (2 / 60)
+
+        isyaTime =
+            (isya + timeZone - (longitude / 15)) + (2 / 60)
+    in
+        { fajr = fajrTime
+        , sunRise = sunRiseTime
+        , dhuhr = dhuhrTime
+        , asr = asrTime
+        , magrib = magribTime
+        , isya = isyaTime
+        }
+
 
 type Direction
-  = CCW
-  | CW
-  | None
+    = CCW
+    | CW
+    | None
+
 
 calcAsrTime : Float -> Float -> Float -> Float -> Float
 calcAsrTime latitude factor julianDate time =
-  let
-      sunPositionAtTime = sunPosition (julianDate + time)
-      declination = sunPositionAtTime.declination
-      absLatDec = (abs (latitude - declination))
-      degreeTan = (tan ((absLatDec * pi) / 180))
-      angle = (-1 * ((atan (1 / (factor + degreeTan)) * 180) / pi))
-  in
-     sunAngleTime latitude angle julianDate time None
+    let
+        sunPositionAtTime =
+            sunPosition (julianDate + time)
+
+        declination =
+            sunPositionAtTime.declination
+
+        absLatDec =
+            (abs (latitude - declination))
+
+        degreeTan =
+            (tan ((absLatDec * pi) / 180))
+
+        angle =
+            (-1 * ((atan (1 / (factor + degreeTan)) * 180) / pi))
+    in
+        sunAngleTime latitude angle julianDate time None
 
 
 sunAngleTime : Float -> Float -> Float -> Float -> Direction -> Float
 sunAngleTime latitude angle julianDate time direction =
-  let
-      sunPositionAtTime = sunPosition (julianDate + time)
-      declination = (sunPositionAtTime.declination)
-      noon = (midDayTime julianDate time)
-      angleVar = (angle)
-      degreeA = (-1 * sin (angleVar * pi / 180))
-      degreeSinD = (sin (declination * pi / 180))
-      degreeSinL = (sin (latitude * pi / 180))
-      degreeCosD = (cos (declination * pi / 180))
-      degreeCosL = (cos (latitude * pi / 180))
-      beforeArcCos = ((degreeA - degreeSinD * degreeSinL) / (degreeCosD * degreeCosL))
-      tVar = (1 / 15 * ((acos beforeArcCos) * 180 / pi))
-  in
-     case direction of
-       CCW -> (noon + (-1 * tVar))
-       _ -> (noon + tVar)
+    let
+        sunPositionAtTime =
+            sunPosition (julianDate + time)
+
+        declination =
+            (sunPositionAtTime.declination)
+
+        noon =
+            (midDayTime julianDate time)
+
+        angleVar =
+            (angle)
+
+        degreeA =
+            (-1 * sin (angleVar * pi / 180))
+
+        degreeSinD =
+            (sin (declination * pi / 180))
+
+        degreeSinL =
+            (sin (latitude * pi / 180))
+
+        degreeCosD =
+            (cos (declination * pi / 180))
+
+        degreeCosL =
+            (cos (latitude * pi / 180))
+
+        beforeArcCos =
+            ((degreeA - degreeSinD * degreeSinL) / (degreeCosD * degreeCosL))
+
+        tVar =
+            (1 / 15 * ((acos beforeArcCos) * 180 / pi))
+    in
+        case direction of
+            CCW ->
+                (noon + (-1 * tVar))
+
+            _ ->
+                (noon + tVar)
+
 
 midDayTime : Float -> Float -> Float
 midDayTime julianDate time =
-  let
-      sunPositionDhuhr = sunPosition (julianDate + time)
-      equationOfTime = sunPositionDhuhr.equation
-  in
-     fixHour (12 - equationOfTime)
+    let
+        sunPositionDhuhr =
+            sunPosition (julianDate + time)
+
+        equationOfTime =
+            sunPositionDhuhr.equation
+    in
+        fixHour (12 - equationOfTime)
+
 
 riseSetAngle : Float -> Float
 riseSetAngle elevation =
-  let
-      angle = 0.0347 * (sqrt (elevation))
-  in
-     0.833 + angle
+    let
+        angle =
+            0.0347 * (sqrt (elevation))
+    in
+        0.833 + angle
+
 
 monthToInt : Date.Month -> Int
 monthToInt month =
-  case month of
-    Date.Jan -> 1
-    Date.Feb -> 2
-    Date.Mar -> 3
-    Date.Apr -> 4
-    Date.May -> 5
-    Date.Jun -> 6
-    Date.Jul -> 7
-    Date.Aug -> 8
-    Date.Sep -> 9
-    Date.Oct -> 10
-    Date.Nov -> 11
-    Date.Dec -> 12
+    case month of
+        Date.Jan ->
+            1
+
+        Date.Feb ->
+            2
+
+        Date.Mar ->
+            3
+
+        Date.Apr ->
+            4
+
+        Date.May ->
+            5
+
+        Date.Jun ->
+            6
+
+        Date.Jul ->
+            7
+
+        Date.Aug ->
+            8
+
+        Date.Sep ->
+            9
+
+        Date.Oct ->
+            10
+
+        Date.Nov ->
+            11
+
+        Date.Dec ->
+            12
+
 
 julianDateByCoordinate : Float -> Float -> Float
 julianDateByCoordinate longitude julianDate =
-  julianDate - (longitude / (15 * 24))
+    julianDate - (longitude / (15 * 24))
+
 
 toJulianDate : Date -> Float
 toJulianDate date =
-  let
-      year = Date.year date
+    let
+        year =
+            Date.year date
 
-      month = monthToInt (Date.month date)
+        month =
+            monthToInt (Date.month date)
 
-      day = Date.day date
+        day =
+            Date.day date
 
-      julianYear =
-        if month <= 2 then
-          year - 1
-        else
-          year
+        julianYear =
+            if month <= 2 then
+                year - 1
+            else
+                year
 
-      julianMonth =
-        if month <= 2 then
-          month + 12
-        else
-          month
+        julianMonth =
+            if month <= 2 then
+                month + 12
+            else
+                month
 
-      aVar =
-        floor (toFloat (julianYear) / 100)
+        aVar =
+            floor (toFloat (julianYear) / 100)
 
-      bVar =
-        2 - aVar + floor (toFloat (aVar) / 4)
+        bVar =
+            2 - aVar + floor (toFloat (aVar) / 4)
+    in
+        toFloat (floor (365.25 * toFloat (julianYear + 4716)) + floor (30.6001 * toFloat (julianMonth + 1))) + toFloat day + toFloat bVar - 1524.5
 
-  in
-     toFloat (floor (365.25 * toFloat (julianYear + 4716)) + floor (30.6001 * toFloat (julianMonth + 1))) + toFloat day + toFloat bVar - 1524.5
 
-type alias SunPosition = { declination: Float, equation: Float }
+type alias SunPosition =
+    { declination : Float, equation : Float }
+
 
 sunPosition : Float -> SunPosition
 sunPosition julianDate =
-  let
-      numberOfDays = julianDate - 2451545.0
+    let
+        numberOfDays =
+            julianDate - 2451545.0
 
-      meanAnomaly =
-        fixAngle (357.529 + (0.98560028 * numberOfDays))
+        meanAnomaly =
+            fixAngle (357.529 + (0.98560028 * numberOfDays))
 
-      meanLongitude =
-        fixAngle (280.459 + (0.98564736 * numberOfDays))
+        meanLongitude =
+            fixAngle (280.459 + (0.98564736 * numberOfDays))
 
-      eclipticLongitude =
-        let
-            x =
-              1.915 * (sin (degrees (meanAnomaly)))
-            y =
-              0.020 * (sin (degrees (2 * meanAnomaly)))
-        in
-           fixAngle (meanLongitude + x + y)
+        eclipticLongitude =
+            let
+                x =
+                    1.915 * (sin (degrees (meanAnomaly)))
 
-      distanceOfSun =
-        1.00014 - (0.01671 * cos (degrees (meanAnomaly))) - (0.00014 * cos (degrees (2 * meanAnomaly)))
+                y =
+                    0.02 * (sin (degrees (2 * meanAnomaly)))
+            in
+                fixAngle (meanLongitude + x + y)
 
-      obliquityOfTheElliptic =
-        23.439 - (0.00000036 * numberOfDays)
+        distanceOfSun =
+            1.00014 - (0.01671 * cos (degrees (meanAnomaly))) - (0.00014 * cos (degrees (2 * meanAnomaly)))
 
-      angularCorrection =
-        let
-            y =
-              cos (degrees (obliquityOfTheElliptic)) * sin (degrees (eclipticLongitude))
-            x =
-              cos (degrees (eclipticLongitude))
-            arctan2 =
-              ((atan2 y x) * 180.0) / pi
-        in
-           arctan2 / 15.0
+        obliquityOfTheElliptic =
+            23.439 - (0.00000036 * numberOfDays)
 
-      equation =
-        meanLongitude / 15.0 - fixHour (angularCorrection)
+        angularCorrection =
+            let
+                y =
+                    cos (degrees (obliquityOfTheElliptic)) * sin (degrees (eclipticLongitude))
 
-      declination =
-        (asin (sin (degrees (obliquityOfTheElliptic)) * sin (degrees (eclipticLongitude)))) * 180 / pi
-  in
-     { declination = declination, equation = equation }
+                x =
+                    cos (degrees (eclipticLongitude))
+
+                arctan2 =
+                    ((atan2 y x) * 180.0) / pi
+            in
+                arctan2 / 15.0
+
+        equation =
+            meanLongitude / 15.0 - fixHour (angularCorrection)
+
+        declination =
+            (asin (sin (degrees (obliquityOfTheElliptic)) * sin (degrees (eclipticLongitude)))) * 180 / pi
+    in
+        { declination = declination, equation = equation }
+
 
 fix : Float -> Float -> Float
 fix a b =
-  let
-      aVar = a - (b * toFloat (floor (a / b)))
-  in
-     if aVar < 0 then
-       aVar + b
-     else
-       aVar
+    let
+        aVar =
+            a - (b * toFloat (floor (a / b)))
+    in
+        if aVar < 0 then
+            aVar + b
+        else
+            aVar
+
 
 fixAngle : Float -> Float
 fixAngle a =
-  fix a 360.0
+    fix a 360.0
+
 
 fixHour : Float -> Float
 fixHour a =
-  fix a 24.0
+    fix a 24.0
