62b64139a114fe40fcbadff32343c93471cbb6ef
Tito Pandu Brahmanto <tito@pandubrahmanto.com>
2017-01-24 13:13:30 +0700

    Fix button styling safari

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
 */
 
 importScripts('./serviceworker-cache-polyfill.js');
-var CACHE_VERSION = 16;
+var CACHE_VERSION = 17;
 var CURRENT_CACHES = {
   prefetch: 'prefetch-cache-v' + CACHE_VERSION
 };
